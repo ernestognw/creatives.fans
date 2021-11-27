c17e95a4f573cfd094e4a518c86d79faecc764ff
ernestognw <ernestognw@gmail.com>
2021-11-26 21:18:23 -0600

    Feature: Added extra fields to user query

--- src/graphql/resolvers/user/queries/index.js
+++ src/graphql/resolvers/user/queries/index.js
@@ -1,14 +1,76 @@
-import { User } from '@db/models';
+import { User, Support } from '@db/models';
 import { defaultParams } from '@config/constants';
 import { buildQuery, buildSearch } from '@graphql/resolvers/utils';
 
 const userQueries = {
-  users: async (_, { id, params = defaultParams, search = {}, sortBy }) => {
+  users: async (_, { id, params = defaultParams, search = {}, sortBy, supports, supportedBy }) => {
     const query = {
       deleted: false,
       ...buildQuery(id, '_id'),
       ...buildSearch(search),
     };
+
+    if (supports) {
+      const $match = {
+        deleted: false,
+        ...buildQuery(supports, 'creative', { aggregation: true }),
+      };
+
+      // Better to do in an aggregation.
+      // Otherwise ids should be calculated in a .map in node memory.
+      // mongo atlas memory > server memory
+      const results = await Support.aggregate([
+        {
+          // Get subconceptInstances
+          $match,
+        },
+        {
+          // Group them
+          $group: {
+            _id: null,
+            ids: {
+              $addToSet: { $toString: '$fan' },
+            },
+          },
+        },
+      ]);
+
+      query._id = {
+        ...(query._id ?? {}), // Respect old constructed query
+        $in: [...(query._id?.$in ?? []), ...(results?.[0]?.ids ?? [])], // Respect old constructed $in
+      };
+    }
+
+    if (supportedBy) {
+      const $match = {
+        deleted: false,
+        ...buildQuery(supportedBy, 'fan', { aggregation: true }),
+      };
+
+      // Better to do in an aggregation.
+      // Otherwise ids should be calculated in a .map in node memory.
+      // mongo atlas memory > server memory
+      const results = await Support.aggregate([
+        {
+          // Get subconceptInstances
+          $match,
+        },
+        {
+          // Group them
+          $group: {
+            _id: null,
+            ids: {
+              $addToSet: { $toString: '$creative' },
+            },
+          },
+        },
+      ]);
+
+      query._id = {
+        ...(query._id ?? {}), // Respect old constructed query
+        $in: [...(query._id?.$in ?? []), ...(results?.[0]?.ids ?? [])], // Respect old constructed $in
+      };
+    }
 
     return {
       results: async () => {
