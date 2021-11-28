c5f633a3160065c409403d3d02650d0b69c37fc2
ernestognw <ernestognw@gmail.com>
2021-11-27 20:06:55 -0600

    Fix: Stripe setup intent

--- src/graphql/resolvers/stripe/queries/index.js
+++ src/graphql/resolvers/stripe/queries/index.js
@@ -14,13 +14,13 @@ const stripeQueries = {
       await user.save();
     }
 
-    const StripeSetupIntent = await stripe.StripeSetupIntents.create({
+    const setupIntent = await stripe.setupIntents.create({
       customer: user.stripeCustomerId,
       payment_method_types: ['card'],
     });
 
     return {
-      clientSecret: StripeSetupIntent.client_secret,
+      clientSecret: setupIntent.client_secret,
     };
   },
 };
