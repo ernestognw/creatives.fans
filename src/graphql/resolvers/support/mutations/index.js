d866acc42afeb35a743aff61c579ef0e2371545e
ernestognw <ernestognw@gmail.com>
2021-11-30 21:55:30 -0600

    Feature: Added payment processing

--- src/graphql/resolvers/support/mutations/index.js
+++ src/graphql/resolvers/support/mutations/index.js
@@ -1,7 +1,33 @@
-import { Support } from '@db/models';
+import { Support, User } from '@db/models';
+import stripe from '@connections/stripe';
 
 const supportMutations = {
-  createSupport: async (_, { support }, { user: { id: fan } }) => {
+  createSupport: async (_, { support: supportArg }, { user: { id: fan } }) => {
+    const support = { ...supportArg };
+    if (support.paymentMethod) {
+      if (!support.amount) throw new Error('Amount required when payment method is provided');
+
+      const paymentMethod = await stripe.paymentMethods.retrieve(support.paymentMethod);
+
+      const user = await User.findOne({
+        _id: fan,
+        stripeCustomerId: paymentMethod.customer,
+      });
+
+      if (!user) throw new Error('Payment method is not associated to user');
+
+      await stripe.paymentIntents.create({
+        amount: support.amount * 100,
+        currency: 'mxn',
+        customer: paymentMethod.customer,
+        payment_method: paymentMethod.id,
+        off_session: true,
+        confirm: true,
+      });
+
+      delete support.paymentMethod;
+    } else if (support.amount) throw new Error('Payment method required when amount is provided');
+
     const newSupport = new Support({
       ...support,
       fan,
